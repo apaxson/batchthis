f09cb9cc55a9623a672dccd869ff688c9a272232
= <aj@thepaxson5.org>
2020-03-04 03:23:43 -0600

    Added supporting html assets (css,js) and templates for initial dashboard

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from batchthis.models import Batch
+from batchthis.models import Batch, Fermenter
 # Create your views here.
 
 def index(request):
@@ -8,7 +8,14 @@ def index(request):
     top_batches = Batch.objects.all()[:5]
     total_batch_count = Batch.objects.all().count()
     active_batch_count = Batch.objects.filter(active=True).count()
-    active_fermenters = Fermenter
-    return HttpResponse('Hello.  This is my first page.')
+    active_fermenters = Fermenter.objects.filter(status=Fermenter.STATUS_ACTIVE)
+
+    context = {
+        'top_batches': top_batches,
+        'total_batch_count': total_batch_count,
+        'active_batch_count': active_batch_count,
+        'active_fermenters': active_fermenters
+    }
+    return render(request,'index.html',context=context)
 
 
