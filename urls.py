8bcbe4d428f4a454478a2f9ba7be5c1a16ba96b0
= <aj@thepaxson5.org>
2020-03-15 15:31:32 -0500

    Fixed missing URL for new Addon method.

--- urls.py
+++ urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('addTest', views.batchTest, name='addTest'),
+    path('addAddon', views.batchAddition, name='addAddon'),
+    path('addNote', views.batchNote, name='addNote'),
     path('batch/<int:pk>', views.batch, name='batch'),
     path('batch/<int:pk>/addTest', views.batchTest, name='addDetailTest'),
     path('batch/<int:pk>/addNote', views.batchNote, name='addDetailNote'),
+    path('batch/<int:pk>/addAddon', views.batchAddition, name="addDetailAddon"),
     path('batch/<int:pk>/batchGraphs', views.batchGraphs, name='batchGraphs')
 ]
