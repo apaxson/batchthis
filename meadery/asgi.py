08035e8a1efd4c7eb63352de61800d726c1a3025
Aaron Paxson <aj@thepaxson5.org>
2026-09-11 08:58:56 -0500

    Refactored directory structure to a more generic root application name.

--- meadery/asgi.py
+++ meadery/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for stonesrivermeadery project.
+ASGI config for meadery project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stonesrivermeadery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meadery.settings')
 
 application = get_asgi_application()
--- meadery/wsgi.py
+++ meadery/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stonesrivermeadery project.
+WSGI config for meadery project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stonesrivermeadery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meadery.settings')
 
 application = get_wsgi_application()
