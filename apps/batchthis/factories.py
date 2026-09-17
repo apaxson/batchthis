f7f6a9f957c60670620a8f90cd80d75a5cd70475
Aaron Paxson <aj@thepaxson5.org>
2026-09-17 18:20:19 -0400

    Fixed bug in Specific Gravity charting when creating a new Batch.

--- apps/batchthis/factories.py
+++ apps/batchthis/factories.py
@@ -1,7 +1,21 @@
 import factory
 from pint import Quantity
 
-from .models import Adjunct, AdjunctType, AdjunctUsage, Fermentable, FermentableType, Yeast
+from .models import (
+    Adjunct,
+    AdjunctType,
+    AdjunctUsage,
+    BatchCategory,
+    BatchStyle,
+    BatchTestType,
+    Fermentable,
+    FermentableType,
+    Fermenter,
+    Recipe,
+    Unit,
+    Vessel,
+    Yeast,
+)
 
 
 class FermentableTypeFactory(factory.django.DjangoModelFactory):
@@ -58,3 +72,70 @@ class YeastFactory(factory.django.DjangoModelFactory):
     max_temp = Quantity(24, "degC")
     flocculation = "Medium"
     attenuation = 75.0
+
+
+class UnitFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Unit
+
+    identifier = "sg"
+    label = "SG"
+    name = "Specific Gravity"
+    category = Unit.CONCENTRATION
+
+
+class VesselFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Vessel
+
+    name = factory.Sequence(lambda n: f"Vessel{n}")
+    max_size = 6
+    max_size_units = factory.SubFactory(UnitFactory, identifier="gal", label="gal", name="Gallons", category=Unit.VOLUME)
+    status = Vessel.STATUS_READY
+    intended_use = "PRI"
+
+
+class FermenterFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Fermenter
+
+    vessel = factory.SubFactory(VesselFactory)
+
+
+class BatchTestTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BatchTestType
+
+    name = "Specific Gravity"
+    shortid = "specific-gravity"
+
+
+class BatchStyleFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BatchStyle
+
+    name = factory.Sequence(lambda n: f"Style{n}")
+
+
+class BatchCategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BatchCategory
+
+    name = factory.Sequence(lambda n: f"Category{n}")
+    style = factory.SubFactory(BatchStyleFactory)
+    bjcp_code = "A1"
+
+
+class RecipeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Recipe
+
+    name = factory.Sequence(lambda n: f"Recipe{n}")
+    dateCreated = factory.Faker("date")
+    category = factory.SubFactory(BatchCategoryFactory)
+    brewer = "Test Brewer"
+    batchSize = Quantity(6, "gallons")
+    notes = ""
+    estOG = Quantity(1.09, "sg")
+    estFG = Quantity(1.0, "sg")
+    estABV = 12.0
